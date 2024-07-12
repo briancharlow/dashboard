79c485f5073fd54ffe03ee06eee4361b10e79c73
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-07-12 22:57:00 +0300

    configures app.js to accept the theme changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+    const [theme, colorMode] = useMode();
     return (
-        <div className="app">
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <main className="content">
 
-        </div>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 }
+
 
 export default App;
