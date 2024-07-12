f2518762913236948f4470f99c474aefe92d4666
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-07-12 21:48:05 +0300

    creates the toggle function for switching modes

--- src/theme.js
+++ src/theme.js
@@ -198,4 +198,15 @@ export const colorModeContext = createContext({
 });
 
 export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () =>
+                setMode((prev) => (prev === "light" ? "dark" : "light"))
+        }),
+        []
+    );
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return [theme, colorMode];
 }
