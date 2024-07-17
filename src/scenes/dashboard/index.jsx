15a715d6e8c956f9e35771b7b65263a167f73b0d
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-07-18 00:18:32 +0300

    adds the header content for dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+  return;
+  <Box m="20px">
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  </Box>;
 };
 
 export default Dashboard;
