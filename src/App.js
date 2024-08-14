cb72817ba9eab808a739459eb6939e43bc360020
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-08-14 23:42:56 +0300

    imports calender component correctly

--- src/App.js
+++ src/App.js
@@ -33,13 +33,14 @@ function App() {
                             <Route path="/team" element={<Team />} />
                             <Route path="/invoices" element={<Invoices />} />
                             <Route path="/contacts" element={<Contacts />} />
+                           <Route path="/calendar" element={<Calendar />} />
                             {/* <Route path="/bar" element={<Bar />} /> */}
                             <Route path="/form" element={<Form />} />
                             {/* <Route path="/line" element={<Line />} /> */}
                             {/* <Route path="/pie" element={<Pie />} /> */}
                             <Route path="/faq" element={<FAQ />} />
                             {/* <Route path="/geography" element={<Geography />} /> */}
-                            <Route path="/calender" element={<Calender />} />
+
                         </Routes>
                     </main>
                 </div>
